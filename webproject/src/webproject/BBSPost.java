07e2ece74c051ba057ef7650f836a5cd431f4f94
daelonge <dleogus0569@naver.com>
2019-04-16 21:01:47 +0900

    1

--- webproject/src/webproject/BBSPost.java
+++ webproject/src/webproject/BBSPost.java
@@ -5,32 +5,38 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import Model.BBSPostModel;
-
 public class BBSPost extends HttpServlet{
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		request.setCharacterEncoding("euc-kr");
 		String name = (String)request.getParameter("NAME");
-		String title = (String)request.getParameter("TITLE");
+		String title = request.getParameter("TITLE");
 		String content = (String)request.getParameter("CONTENT");
 		Date date = new Date();
 		Long time = date.getTime();
 		String filename = time + ".txt";
 		
-		BBSPostModel model = new BBSPostModel();
-		model.setName(name);
-		model.setTitle(title);
-		model.setContent(content);
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("NAME", name);
-		session.setAttribute("TITLE", title);
-		session.setAttribute("CONTENT", content);
-		session.setAttribute("FILENAME", filename);
 		response.setContentType("text/html;charset=euc-kr");
-		PrintWriter out = response.getWriter();
-		RequestDispatcher dispatcher = request.getRequestDispatcher("BBSPost2.jsp");
-		dispatcher.forward(request, response);
+		String result;
+		ServletContext context = getServletContext();
+		PrintWriter writer = null;
+		try{
+			String filePath = context.getRealPath("/WEB-INF/bbs/" + filename);
+			writer = new PrintWriter(filePath);
+			writer.println("이름 : " + name);
+			writer.println("제목 : " + title);
+			writer.println("내용 : " + content);
+			result = "SUCCESS";
+		}catch(IOException ioe){
+			result = "FAIL";
+		}
+		finally{
+			try{
+				writer.close();
+			}
+			catch(Exception e){
+			}
+		}
+		response.sendRedirect("BBSPostResult.jsp?RESULT=" + result);
 	}
 
 }
--- webproject/src/webproject/FourRulesServlet.java
+++ webproject/src/webproject/FourRulesServlet.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import Model.FourRulesModel;
-
 public class FourRulesServlet extends HttpServlet{
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		request.setCharacterEncoding("euc-kr");
@@ -14,28 +12,31 @@ public class FourRulesServlet extends HttpServlet{
 		String str2 = request.getParameter("NUM2");
 		int num1 = Integer.parseInt(str1);
 		int num2 = Integer.parseInt(str2);
-
-		FourRulesModel model = new FourRulesModel();
-		model.setNum1(num1);
-		model.setNum2(num2);
-		model.setSum(new Integer(num1 + num2));
-		model.setDelete(new Integer(num1 - num2));
-		model.setProduct(new Integer(num1*num2));
-		model.setQuotient(new Integer(num1/num2));
-
-		HttpSession session = request.getSession();
-		session.setAttribute("NUM1", Integer.toString(num1));
-		session.setAttribute("NUM2", Integer.toString(num2));
-		session.setAttribute("SUM", Integer.toString(new Integer(num1 + num2)));
-		session.setAttribute("DELETE", Integer.toString(new Integer(num1 - num2)));
-		session.setAttribute("PRODUCT", Integer.toString(new Integer(num1*num2)));
-		session.setAttribute("QUOTIENT", Integer.toString(new Integer(num1/num2)));
-		//session.setAttribute("FILENAME", filename);
-		response.setContentType("text/html;charset=euc-kr");
-		PrintWriter out = response.getWriter();
-		RequestDispatcher dispatcher = request.getRequestDispatcher("FourRules2.jsp");
-		dispatcher.forward(request, response);
 		
+		String result;
+		ServletContext context = getServletContext();
+		PrintWriter writer = null;
+		try{
+			String filePath = context.getRealPath("/WEB-INF/bbs/answer.txt");
+			writer = new PrintWriter(filePath);
+			writer.println("첫 번째 수 : " + num1);
+			writer.println("두 번째 수 : " + num2);
+			writer.println("더한 결과 : " + new Integer(num1+num2));
+			writer.println("뺀 결과 : " + new Integer(num1-num2));
+			writer.println("곱한 결과 : " + new Integer(num1*num2));
+			writer.println("나눈 결과 : " + new Integer(num1/num2));
+			result = "SUCCESS";
+		}catch(IOException ioe){
+			result = "FAIL";
+		}
+		finally{
+			try{
+				writer.close();
+			}
+			catch(Exception e){
+			}
+		}
+		response.sendRedirect("FourRulesResult.jsp?RESULT=" + result);
 	}
 
 }
